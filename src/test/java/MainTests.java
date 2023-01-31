19691693b36acabaff22958d3c55ef35f47e7ed9
Anastasya <03.nastya@mail.ru>
2023-01-31 17:41:58 +0400

    second

--- src/test/java/MainTests.java
+++ src/test/java/MainTests.java
@@ -10,7 +10,7 @@ public class MainTests {
     @Test
     public void testNewStrings() {
         //arrange
-        String string = "анна, aндрей, александр, пётр", expected = "АННА, АНДРЕЙ, АЛЕКСАНДР, ПЁТР";
+        String expected = "АННА, АНДРЕЙ, АЛЕКСАНДР, ПЁТР";
         //act
         var result = sut.newStrings();
         //assert
@@ -21,7 +21,6 @@ public class MainTests {
     @Test
     public void testIsTrue() {
         //arrange
-        int first = 567, second = 22;
         boolean expected = false;
         // act
         var result = sut.isTrue();
@@ -32,7 +31,6 @@ public class MainTests {
     @Test
     public void testNewStringArray() {
         //arrange
-        List<String> list = Arrays.asList("Анастасия Дмитриевна", "Пётр Олегович", "Юлия Петровна");
         List<String> expected = Arrays.asList("Анастасия Дмитриевна", "Пётр Олегович", "Юлия Петровна", "Иванов Иван");
         //act
         var result = sut.newStringArray();
